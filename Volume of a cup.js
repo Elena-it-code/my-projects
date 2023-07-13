12caac6025831736d402d0738cddae5766c453f4
ElenaKuklina <cuklina.lena@gmail.com>
2023-07-13 21:06:15 +0300

    Add files via upload

---
+++ Volume of a cup.js
@@ -0,0 +1,28 @@
+// Your job is to return the volume of a cup when given the diameter of the top, the diameter of the bottom and the height.
+
+// You know that there is a steady gradient from the top to the bottom.
+
+// You want to return the volume rounded to 2 decimal places.
+
+// Exmples:
+
+// cupVolume(1, 1, 1);
+// //returns 0.79
+
+// cupVolume(10, 8, 10);
+// //returns 638.79
+
+// cupVolume(1000, 1000, 1000);
+// //returns 785398163.4
+
+// cupVolume(13.123, 123.12, 1);
+// //returns 4436.57
+
+// cupVolume(5, 12, 31);
+// //returns 1858.51
+
+function cupVolume(d1, d2, height){
+  let num;
+  num = ((Math.PI*height)/12) * ((d2*d2)+(d2*d1)+(d1*d1));
+  return Number(num.toFixed(2));
+}
